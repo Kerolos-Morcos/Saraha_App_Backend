e9a4e108939342871e01b21bec486b9ec1c6d7b8
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2026-09-17 02:30:01 +0300

    Edit Get User Profile By Id

--- src/Modules/User/user.service.js
+++ src/Modules/User/user.service.js
@@ -4,12 +4,12 @@ import { decrypt } from "../../Utils/Security/encryption.security.js";
 
 // GET user profile by id
 export const getUserProfileService = async (userId) => {
+    if (!isValidObjectId(userId)) throw new Error('Invalid user ID');
     const user = await User.findById(userId);
+    if (!user) throw new Error('User not found');
     // Decrypt the phone number before returning the user object
     if (user.phoneNumber)
         user.phoneNumber = decrypt(user.phoneNumber);
-    if (!isValidObjectId(userId)) throw new Error('Invalid user ID');
-    if (!user) throw new Error('User not found');
     return user;
 }
 
